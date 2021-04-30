6fe728d276d833ef6dc54af737ac9d6f974cc9fe
CDVin <cdp.vincent@gmail.com>
2021-04-30 21:13:32 +0200

    Added date function

--- src/app.js
+++ src/app.js
@@ -1,1 +1,11 @@
 
+ return `${day} ${hours}:${minutes}`;
+}
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+  return days[day];
+}
