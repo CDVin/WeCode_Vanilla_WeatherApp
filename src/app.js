103ed4018ff58e4d72aacc4e23c4c0312e6dd805
CDVin <cdp.vincent@gmail.com>
2021-05-06 14:38:53 +0200

    Amendments

--- src/app.js
+++ src/app.js
@@ -60,18 +60,66 @@ event.preventDefault();
 let cityInputElement=document.querySelector("city-input");
 search(cityInputElement.value);
 }
+
+
+function searchCity(city) {
+  let apiKey = "60fdd99ad6f737d97b1a9594e9fd7160";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  let units = "metric";
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+  searchCity(city);
+}
+
+function searchLocation(position) {
+  let apiKey = "60fdd99ad6f737d97b1a9594e9fd7160";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("click", searchCity);
+
 //(city) {;
   let apiKey = "60fdd99ad6f737d97b1a9594e9fd7160";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
  
   axios.get(apiUrl).then(displayTemperature);
+
+function displayFahrenehitTemperature(event) {
+ event.preventDefault();
+  celciusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+}
+function displayCelciusTemperature(event) {
+  event.preventDefault();
+  celciusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(celciusTemperature);
 }
 
-search("New York");
+
+searchCity("New York");
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
+
 let fahrenheitLink = document.querySelector("#fahrenheit-link");
-fahrenheitLink.addEventListener("click", showFahrenehitTemperature);
+fahrenheitLink.addEventListener("click", displayFahrenehitTemperature);
+
 let celciusLink = document.querySelector("#celcius-link");
-celciusLink.addEventListener("click", showCelciusTemperature);
+celciusLink.addEventListener("click", displayCelciusTemperature);
+    
