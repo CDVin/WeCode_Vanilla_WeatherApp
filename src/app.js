e964ff05b352b3d3f254f85d90b912842ccd8869
CDVin <cdp.vincent@gmail.com>
2021-05-08 22:18:48 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -35,34 +35,31 @@ function displayForecast(response) {
 
   let forecastElement = document.querySelector("#forecast");
 
-  let forecastHTML = `<div class="row”>`;
-
+  let forecastHTML = `<div class="row">`;
   forecast.forEach(function (forecastDay, index) {
     if (index < 6) {
       forecastHTML =
         forecastHTML +
         `
-          <div class="col-2">
-            <div class="weather-forecast-date">${formatDay(
-              forecastDay.dt
-            )}</div>
-            <img 
-            src="http://openweathermap.org/img/wn/${
-              forecastDay.weather[0].icon
-            }@2x.png"
-            alt=""
-            width="42"
-            />
-          <div class="weather-forecast-temperatures">
-            <span class="weather-forecast-temperature-max"><strong>${Math.round(
-              forecastDay.temp.max
-            )}°</strong> | </span>
-            <span class="weather-forecast-temperatures-min">${Math.round(
-              forecastDay.temp.min
-            )}°</span>
-          </div>
-       </div>
-`;
+      <div class="col-2">
+        <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+        <img
+          src="http://openweathermap.org/img/wn/${
+            forecastDay.weather[0].icon
+          }@2x.png"
+          alt=""
+          width="42"
+        />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max"> ${Math.round(
+            forecastDay.temp.max
+          )}° </span>
+          <span class="weather-forecast-temperature-min"> ${Math.round(
+            forecastDay.temp.min
+          )}° </span>
+        </div>
+      </div>
+  `;
     }
   });
 
